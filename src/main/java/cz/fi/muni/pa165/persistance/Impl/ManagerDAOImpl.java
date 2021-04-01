4eefa4d806cb8a7fc483d3cd34bd8a518be35d2c
Kaláb Jan <Jan.Kalab@ysoft.com>
2021-04-01 23:26:26 +0200

    Manager update

--- src/main/java/cz/fi/muni/pa165/persistance/Impl/ManagerDAOImpl.java
+++ src/main/java/cz/fi/muni/pa165/persistance/Impl/ManagerDAOImpl.java
@@ -1,7 +1,46 @@
 package cz.fi.muni.pa165.persistance.Impl;
 
+import cz.fi.muni.pa165.entities.Manager;
+import cz.fi.muni.pa165.persistance.interfaces.ManagerDAO;
+
+import java.util.List;
+
 /**
  * @author Jan Kaláb
  */
-public class ManagerDAOImpl {
+public class ManagerDAOImpl implements ManagerDAO {
+    @Override
+    public Manager getById(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Manager> getAll() {
+        return null;
+    }
+
+    @Override
+    public void create(Manager manager) {
+
+    }
+
+    @Override
+    public void update(Manager manager) {
+
+    }
+
+    @Override
+    public void remove(Manager manager) {
+
+    }
+
+    @Override
+    public Manager getByUserName(String userName) {
+        return null;
+    }
+
+    @Override
+    public Manager getByName(String name) {
+        return null;
+    }
 }
