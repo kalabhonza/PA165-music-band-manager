72a9b154463b259de52545fb9ef24e11afb0d8c9
244021 <244021@mail.muni.cz>
2021-04-04 21:12:56 +0200

    Band and song entities, dao and dao impl

--- src/main/java/cz/fi/muni/pa165/entities/Song.java
+++ src/main/java/cz/fi/muni/pa165/entities/Song.java
@@ -1,7 +1,72 @@
 package cz.fi.muni.pa165.entities;
 
+import javax.persistence.Column;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Time;
+import java.util.Objects;
+
 /**
  * @author Igor Ignác
  */
 public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private Band band;
+
+    @Column(nullable = false)
+    private Time duration;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Band getBand() {
+        return band;
+    }
+
+    public void setBand(Band band) {
+        this.band = band;
+    }
+
+    public Time getDuration() {
+        return duration;
+    }
+
+    public void setDuration(Time duration) {
+        this.duration = duration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Song song = (Song) o;
+        return name.equals(song.name) && band.equals(song.band);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getName(), getBand());
+    }
 }
--- src/main/java/cz/fi/muni/pa165/persistance/interfaces/SongDAO.java
+++ src/main/java/cz/fi/muni/pa165/persistance/interfaces/SongDAO.java
@@ -1,7 +1,57 @@
 package cz.fi.muni.pa165.persistance.interfaces;
 
+import cz.fi.muni.pa165.entities.Band;
+import cz.fi.muni.pa165.entities.Song;
+
+import java.util.List;
+
 /**
  * @author Igor Ignác
  */
 public interface SongDAO {
+    /**
+     * Saves song into database
+     * @param song song object which should be saved
+     */
+    void create(Song song);
+
+    /**
+     * Updates song in database
+     * @param song song which should be updated
+     * @return updated song
+     */
+    Song update(Song song);
+
+    /**
+     * Search database for song matching given ID
+     * @param id song's ID
+     * @return found song or null
+     */
+    Song findSongById(Long id);
+
+    /**
+     * Search database for song matching given name
+     * @param name song's name
+     * @return found song or null
+     */
+    Song findSongByName(String name);
+
+    /**
+     * Search database for song matching given Band
+     * @param band song's band
+     * @return found song or null
+     */
+    Song findSongByBand(Band band);
+
+    /**
+     * Returns all songs in database
+     * @return all songs in database
+     */
+    List<Song> findAll();
+
+    /**
+     * Deletes song from database
+     * @param song song to be deleted
+     */
+    void deleteSong(Song song);
 }
