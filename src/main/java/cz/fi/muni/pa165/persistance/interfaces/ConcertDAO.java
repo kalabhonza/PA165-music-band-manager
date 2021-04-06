f49068fecd600d0c2eb820f3e707254a90e6da58
Ales Paroulek <456555@mail.muni.cz>
2021-04-06 16:30:09 +0200

    added tests for album and song, added javadoc to concertDAO and musicianDAO

--- src/main/java/cz/fi/muni/pa165/persistance/interfaces/ConcertDAO.java
+++ src/main/java/cz/fi/muni/pa165/persistance/interfaces/ConcertDAO.java
@@ -11,12 +11,49 @@ import java.util.List;
  */
 public interface ConcertDAO {
 
+    /**
+     * Finds concert by its id
+     * @param id Concert's id
+     * @return Concert with given id or null if it doesn't exist
+     */
     Concert findById(long id);
+
+    /**
+     * Finds all concerts
+     * @return All concerts in database
+     */
     List<Concert> findAll();
+
+    /**
+     * Finds all concerts of given band
+     * @param band Band of which concerts will be found
+     * @return Concerts of given band
+     */
     List<Concert> findAllByBand(Band band);
+
+    /**
+     * Finds all concert of given date
+     * @param date Date of which concerts will be found
+     * @return Concerts of given date
+     */
     List<Concert> findAllByDate(LocalDate date);
+
+    /**
+     * Creates a new concert
+     * @param concert Concert to be created
+     */
     void create(Concert concert);
+
+    /**
+     * Updates given concert
+     * @param concert Concert to be updated
+     */
     void update(Concert concert);
+
+    /**
+     * Removes given concert
+     * @param concert Concert to be removed
+     */
     void remove(Concert concert);
 
 }
