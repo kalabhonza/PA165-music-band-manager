e1164630e099ef364f23f5670866cae427305f44
Ales Paroulek <456555@mail.muni.cz>
2021-04-06 22:20:18 +0200

    fixed tests for album and song

--- src/test/java/cz/fi/muni/pa165/persistance/impl/AlbumDAOTest.java
+++ src/test/java/cz/fi/muni/pa165/persistance/impl/AlbumDAOTest.java
@@ -2,13 +2,17 @@ package cz.fi.muni.pa165.persistance.impl;
 
 import cz.fi.muni.pa165.MusicBandManagerApplicationContext;
 import cz.fi.muni.pa165.entities.Album;
-import cz.fi.muni.pa165.persistance.Impl.AlbumDAOImpl;
 import cz.fi.muni.pa165.persistance.interfaces.AlbumDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
+import org.springframework.transaction.annotation.Transactional;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -20,12 +24,12 @@ import java.util.List;
  * @author Aleš Paroulek
  */
 @ContextConfiguration(classes = MusicBandManagerApplicationContext.class)
+@TestExecutionListeners(TransactionalTestExecutionListener.class)
+@Transactional
 public class AlbumDAOTest extends AbstractTestNGSpringContextTests {
     @Autowired
     private AlbumDAO albumDAO;
 
-    @PersistenceUnit
-    private EntityManagerFactory emf;
 
     @PersistenceContext
     private EntityManager em;
@@ -36,100 +40,76 @@ public class AlbumDAOTest extends AbstractTestNGSpringContextTests {
 
     @BeforeMethod
     public void beforeTest() {
-        EntityManager em = null;
+        albumA = new Album();
+        albumA.setName("A");
+        em.persist(albumA);
 
-        try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
+        albumB = new Album();
+        albumB.setName("B");
+        em.persist(albumB);
 
-            albumA = new Album();
-            albumA.setName("A");
-            albumA.setId(1L);
-            em.persist(albumA);
-
-            albumB = new Album();
-            albumB.setName("B");
-            albumB.setId(2L);
-            em.persist(albumB);
-
-            albumC = new Album();
-            albumC.setName("C");
-            albumC.setId(3L);
-            em.persist(albumC);
-
-            em.getTransaction().commit();
-        } finally {
-            if (em != null) em.close();
-        }
+        albumC = new Album();
+        albumC.setName("C");
+        em.persist(albumC);
     }
 
-//    @Test
-//    public void findAlbumById() {
-//        Album resultA = albumDAO.getById(1L);
-//        Album resultB = albumDAO.getById(2L);
-//        Album resultC = albumDAO.getById(3L);
-//
-//        Assert.assertEquals(resultA.getName(), albumA.getName());
-//        Assert.assertEquals(resultB.getName(), albumB.getName());
-//        Assert.assertEquals(resultC.getName(), albumC.getName());
-//    }
+    @Test
+    public void findAlbumById() {
+        Album resultA = albumDAO.findById(albumA.getId());
+        Album resultB = albumDAO.findById(albumB.getId());
+        Album resultC = albumDAO.findById(albumC.getId());
 
-//    @Test
-//    public void findAllAlbums() {
-//        List<Album> albums = albumDAO.getAllAlbums();
-//        Assert.assertEquals(albums.size(), 3);
-//    }
+        Assert.assertEquals(resultA.getName(), albumA.getName());
+        Assert.assertEquals(resultB.getName(), albumB.getName());
+        Assert.assertEquals(resultC.getName(), albumC.getName());
+    }
 
-//    @Test
-//    public void createAlbum() {
-//        Album albumD = new Album();
-//        albumD.setName("D");
-//        albumD.setId(4L);
-//
-//        albumDAO.create(albumD);
-//
-//        Album resultD = albumDAO.getById(4L);
-//        Assert.assertEquals(albumD.getName(), resultD.getName());
-//    }
+    @Test
+    public void findAllAlbums() {
+        List<Album> albums = albumDAO.findAllAlbums();
+        Assert.assertEquals(albums.size(), 3);
+    }
 
-//    @Test
-//    public void updateAlbum() {
-//        albumB.setName("Z");
-//        albumDAO.update(albumB);
-//
-//        Album resultB = albumDAO.getById(2L);
-//        Assert.assertEquals(albumB.getName(), resultB.getName());
-//    }
-//
-//    @Test
-//    public void removeAlbum() {
-//        int beforeDeleteCount = albumDAO.getAllAlbums().size();
-//        albumDAO.remove(albumB);
-//        int afterDeleteCount = albumDAO.getAllAlbums().size();
-//
-//        Assert.assertTrue(beforeDeleteCount > afterDeleteCount);
-//    }
-//
-//    @Test
-//    public void getAlbumsByName() {
-//        List<Album> sameNameAlbums = albumDAO.getAlbumsByName("A");
-//        Assert.assertEquals(sameNameAlbums.size(), 2);
-//    }
+    @Test
+    public void createAlbum() {
+        Album albumD = new Album();
+        albumD.setName("D");
+
+        albumDAO.create(albumD);
+
+        Album resultD = albumDAO.findById(albumD.getId());
+        Assert.assertEquals(albumD.getName(), resultD.getName());
+    }
+
+    @Test
+    public void updateAlbum() {
+        albumB.setName("Z");
+        albumDAO.update(albumB);
+
+        Album resultB = albumDAO.findById(albumB.getId());
+        Assert.assertEquals(albumB.getName(), resultB.getName());
+    }
+
+    @Test
+    public void removeAlbum() {
+        int beforeDeleteCount = albumDAO.findAllAlbums().size();
+        albumDAO.remove(albumB);
+        int afterDeleteCount = albumDAO.findAllAlbums().size();
+
+        Assert.assertTrue(beforeDeleteCount > afterDeleteCount);
+    }
+
+    @Test
+    public void getAlbumsByName() {
+        albumA.setName("test");
+        albumB.setName("test");
+        List<Album> sameNameAlbums = albumDAO.findAlbumsByName("test");
+        Assert.assertEquals(sameNameAlbums.size(), 2);
+    }
 
     @AfterMethod
     public void afterTest() {
-        EntityManager em = null;
-        try {
-            em = emf.createEntityManager();
-            em.getTransaction().begin();
-            em.createQuery("delete from albums").executeUpdate();
-            em.getTransaction().commit();
-        } finally {
-            if (em != null) {
-                em.close();
-            }
-        }
+        em.createQuery("delete from albums").executeUpdate();
     }
-
 
 }
