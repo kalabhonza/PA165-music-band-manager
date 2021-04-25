cf2028798f0ab8a0a101a7352819427a1e229d1a
Ales Paroulek <456555@mail.muni.cz>
2021-04-25 20:10:49 +0200

    added ConcertFacade and MusicianFacade

---
+++ api/src/main/java/cz/fi/muni/pa165/api/facade/ConcertFacade.java
@@ -0,0 +1,49 @@
+package cz.fi.muni.pa165.api.facade;
+
+import cz.fi.muni.pa165.api.dto.ConcertDTO;
+import java.time.LocalDate;
+import java.util.List;
+
+/**
+ * @author Aleš Paroulek
+ */
+public interface ConcertFacade {
+    /**
+     * Finds concert by its id
+     * @param id Concert's id
+     * @return Concert with given id or null if it doesn't exist
+     */
+    ConcertDTO findById(long id);
+
+    /**
+     * Finds all concerts
+     * @return All concerts in database
+     */
+    List<ConcertDTO> findAll();
+
+
+    /**
+     * Finds all concert of given date
+     * @param date Date of which concerts will be found
+     * @return Concerts of given date
+     */
+    List<ConcertDTO> findAllByDate(LocalDate date);
+
+    /**
+     * Creates a new concert
+     * @param concert Concert to be created
+     */
+    void create(ConcertDTO concert);
+
+    /**
+     * Updates given concert
+     * @param concert Concert to be updated
+     */
+    void update(ConcertDTO concert);
+
+    /**
+     * Removes given concert
+     * @param concert Concert to be removed
+     */
+    void remove(ConcertDTO concert);
+}
---
+++ api/src/main/java/cz/fi/muni/pa165/api/facade/MusicianFacade.java
@@ -0,0 +1,55 @@
+package cz.fi.muni.pa165.api.facade;
+
+import cz.fi.muni.pa165.api.dto.BandDTO;
+import cz.fi.muni.pa165.api.dto.MusicianDTO;
+import java.util.List;
+
+/**
+ * @author Aleš Paroulek
+ */
+public interface MusicianFacade {
+    /**
+     * Finds musician by its id
+     * @param id Musician's id
+     * @return Musician with given id or null if it doesn't exist
+     */
+    MusicianDTO findById(long id);
+
+    /**
+     * Finds musician by its username
+     * @param userName Username of a musician
+     * @return Musician with given username or null if it doesn't exist
+     */
+    MusicianDTO findByUserName(String userName);
+
+    /**
+     * Finds all musicians
+     * @return All musicians in database
+     */
+    List<MusicianDTO> findAll();
+
+    /**
+     * Finds all musicians of given band
+     * @param band Band of which musicians will be found
+     * @return Musicians of given band
+     */
+    List<MusicianDTO> findAllByBand(BandDTO band);
+
+    /**
+     * Creates a new musician
+     * @param musician Musician to be created
+     */
+    void create(MusicianDTO musician);
+
+    /**
+     * Updates musician
+     * @param musician Musician to be updated
+     */
+    void update(MusicianDTO musician);
+
+    /**
+     * Removes musician
+     * @param musician Musician to be removed
+     */
+    void remove(MusicianDTO musician);
+}
