ee2d92be734ed101bae0d5befb8dcd5e7920d7fa
Kaláb Jan <Jan.Kalab@ysoft.com>
2021-05-15 09:01:01 +0200

    controllers with errors

--- rest/src/main/java/cz/fi/muni/pa165/rest/controllers/BandController.java
+++ rest/src/main/java/cz/fi/muni/pa165/rest/controllers/BandController.java
@@ -1,6 +1,9 @@
 package cz.fi.muni.pa165.rest.controllers;
 
+import cz.fi.muni.pa165.api.dto.band.BandCreateDTO;
 import cz.fi.muni.pa165.api.dto.band.BandDTO;
+import cz.fi.muni.pa165.api.dto.band.BandUpdateDTO;
+import cz.fi.muni.pa165.api.dto.manager.ManagerDTO;
 import cz.fi.muni.pa165.api.facade.BandFacade;
 import cz.fi.muni.pa165.rest.assemblers.BandResourceAssembler;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,11 +11,9 @@ import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -47,6 +48,73 @@ public class BandController {
             //throw ExceptionSorter.throwException(ex);
         }
     }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Resource<BandDTO>> getById(@PathVariable Long id){
+        try {
+            return new ResponseEntity<>(bandResourceAssembler.toResource(bandFacade.findBandById(id)), HttpStatus.OK);
+        }catch (Exception ex){
+            //throw ExceptionSorter.throwException(ex);
+        }
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Resource<BandDTO>> getByName(@PathVariable String name){
+        try {
+            List<BandDTO> bands = bandFacade.findBandByName(name);
+            List<Resource<BandDTO>> bandsResource = new ArrayList<>();
+            for (BandDTO bandDTO : bands){
+                bandsResource.add(bandResourceAssembler.toResource(bandDTO));
+            }
+            Resources<Resource<BandDTO>> resultResources = new Resources<>(bandsResource);
+            resultResources.add(linkTo(BandController.class).withSelfRel().withType("GET"));
+            return new ResponseEntity<>(resultResources, HttpStatus.OK);
+        }catch (Exception ex){
+            //throw ExceptionSorter.throwException(ex);
+        }
+    }
+
+    @GetMapping(value = "/{id}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Resource<BandDTO>> getByManager(@RequestBody @Valid ManagerDTO managerDTO){
+        try {
+            return new ResponseEntity<>(bandResourceAssembler.toResource(bandFacade.findBandByManager(managerDTO)), HttpStatus.OK);
+        }catch (Exception ex){
+            //throw ExceptionSorter.throwException(ex);
+        }
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Long> createBand(@RequestBody @Valid BandCreateDTO bandCreateDTO){
+        try{
+            return new ResponseEntity<>(bandFacade.createBand(bandCreateDTO), HttpStatus.CREATED);
+        }catch (Exception ex){
+            //throw ExceptionSorter.throwException(ex);
+        }
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> deleteBand(@RequestBody @Valid BandDTO bandDTO){
+        try {
+            bandFacade.deleteBand(bandDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception ex){
+            //throw ExceptionSorter.throwException(ex);
+        }
+    }
+
+    @PutMapping(consumes = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Void> updateBand(@RequestBody @Valid BandUpdateDTO bandUpdateDTO){
+        try {
+            bandFacade.updateBand(bandUpdateDTO);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        }catch (Exception ex){
+            //throw ExceptionSorter.throwException(ex);
+        }
+    }
+
+
+
+
 
 
 }
