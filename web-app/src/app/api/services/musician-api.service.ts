948b538aaadd54e38bc3c9de22c15abe479f416e
244021 <244021@mail.muni.cz>
2021-05-19 15:23:40 +0200

    Merge main

--- web-app/src/app/api/services/musician-api.service.ts
+++ web-app/src/app/api/services/musician-api.service.ts
@@ -36,25 +36,13 @@ export class MusicianApiService {
    * @param id id of searched musician
    */
   getById(id: number): Observable<Musician> {
-    return of( {
-      name: 'Peter',
-      offers: [{
-        id: 2,
-        style: Style.Rock,
-        tours: [],
-        name: "band",
-        members: [],
-        albums: [],
-        manager: {name: "john"}
-      }]
-    });
-    // return this.http
-    //   .get<MusicianDTO>(`${this.javaRestEndpoint}/musicians/${id}`, {
-    //     headers: MusicianApiService.createDefaultHeaders()
-    //   })
-    //   .pipe(
-    //     map((response) => MusicianMapper.fromDTO(response))
-    //   );
+    return this.http
+      .get<MusicianDTO>(`${this.javaRestEndpoint}/musicians/${id}`, {
+        headers: MusicianApiService.createDefaultHeaders()
+      })
+      .pipe(
+        map((response) => MusicianMapper.fromDTO(response))
+      );
   }
 
   /**
---
+++ web-app/src/app/model/concert.ts
@@ -0,0 +1,3 @@
+export class Concert {
+  name: string;
+}
