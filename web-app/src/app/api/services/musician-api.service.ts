da6686a688a172a0897d4fc504885ba0980b6569
Ales Paroulek <456555@mail.muni.cz>
2021-05-19 18:38:35 +0200

    updated musician-api service

--- web-app/src/app/api/services/musician-api.service.ts
+++ web-app/src/app/api/services/musician-api.service.ts
@@ -7,6 +7,7 @@ import {MusicianMapper} from '../mappers/musician-mapper';
 import {MusicianDTO} from '../dtos/musician-dto';
 import {Style} from '../../model/style';
 
+
 @Injectable()
 export class MusicianApiService {
 
@@ -35,24 +36,81 @@ export class MusicianApiService {
    * @param id id of searched musician
    */
   getById(id: number): Observable<Musician> {
-    return of( {
-      name: 'Peter',
-      offers: [{
-        id: 2,
-        style: Style.Rock,
-        tours: [],
-        name: "band",
-        members: [],
-        albums: [],
-        manager: {name: "john"}
-      }]
-    });
-    // return this.http
-    //   .get<MusicianDTO>(`${this.javaRestEndpoint}/musicians/${id}`, {
-    //     headers: MusicianApiService.createDefaultHeaders()
-    //   })
-    //   .pipe(
-    //     map((response) => MusicianMapper.fromDTO(response))
-    //   );
+    return this.http
+      .get<MusicianDTO>(`${this.javaRestEndpoint}/musician/${id}`, {
+        headers: MusicianApiService.createDefaultHeaders()
+      })
+      .pipe(
+        map((response) => MusicianMapper.fromDTO(response))
+      );
   }
+
+  /**
+   * Sends http request to delete a musician
+   * @param id id of musician to be deleted
+   */
+  delete(id: number): Observable<any> {
+    return this.http
+      .delete(`${this.javaRestEndpoint}/musician/${id}`, {
+        headers: MusicianApiService.createDefaultHeaders()
+      });
+  }
+
+  /**
+   * Sends http request to create a musician
+   * @param musician musician to be created
+   */
+  create(musician: Musician): Observable<Musician> {
+    return this.http
+      .post<MusicianDTO>(
+        `${this.javaRestEndpoint}/musician`,
+        MusicianMapper.toDTO(musician),
+        { headers: MusicianApiService.createDefaultHeaders() }
+      )
+      .pipe(
+        map((response) => MusicianMapper.fromDTO(response))
+      );
+  }
+
+  /**
+   * Sends http request to update a musician
+   * @param musician musician to be updated
+   */
+  update(musician: Musician): Observable<Musician> {
+    return this.http
+      .put<MusicianDTO>(
+        `${this.javaRestEndpoint}/musician`,
+        MusicianMapper.toDTO(musician),
+        { headers: MusicianApiService.createDefaultHeaders() }
+      )
+      .pipe(
+        map((response) => MusicianMapper.fromDTO(response))
+      );
+  }
+
+  // /**
+  //  * Sends http request to retrieve musician by given id
+  //  * @param id id of searched musician
+  //  */
+  // getById(id: number): Observable<Musician> {
+  //   return of( {
+  //     name: 'Peter',
+  //     offers: [{
+  //       id: 2,
+  //       style: Style.Rock,
+  //       tours: [],
+  //       name: "band",
+  //       members: [],
+  //       albums: [],
+  //       manager: {name: "john"}
+  //     }]
+  //   });
+  //   // return this.http
+  //   //   .get<MusicianDTO>(`${this.javaRestEndpoint}/musicians/${id}`, {
+  //   //     headers: MusicianApiService.createDefaultHeaders()
+  //   //   })
+  //   //   .pipe(
+  //   //     map((response) => MusicianMapper.fromDTO(response))
+  //   //   );
+  // }
 }
