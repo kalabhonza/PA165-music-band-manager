fbff2416543f94f907b92161aadf92f949caea92
Ales Paroulek <456555@mail.muni.cz>
2021-05-19 18:12:47 +0200

    updated musician model

--- web-app/src/app/model/musician.ts
+++ web-app/src/app/model/musician.ts
@@ -1,7 +1,11 @@
 import {Band} from './band';
 
 export class Musician {
+  id: number;
   name: string;
+  username: string;
+  password: string;
+  // TO DO: instruments
   offers: Band[];
   band?: Band;
 }
